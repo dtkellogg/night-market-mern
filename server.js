911952b39ef8506b04dc46d7d6db6ceab76eb32f
Toshi <dtkellogg10@gmail.com>
2020-09-03 12:04:05 -0700

    Trying heroko again w a diff approach

--- server.js
+++ server.js
@@ -22,12 +22,13 @@ const app = express();
 // // app.engine("handlebars", exphbs());
 // // app.set("view engine", "handlebars");
 
-  if (process.env.NODE_ENV === 'production') {
-// Exprees will serve up production assets
-app.use(express.static('client/build'));
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+// Set static folder
+app.use(express.static('app/NM2/build'));
 
 // Express serve up index.html file if it doesn't recognize route
-const path = require('path');
+// const path = require('path');
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'app', 'NM2', 'build', 'index.html'));
 });
