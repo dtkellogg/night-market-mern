74516ab92fbe21d108962207a3ce8a71e8d2f837
Toshi <dtkellogg10@gmail.com>
2020-09-03 01:06:41 -0700

    build path rewritten w NM2

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(express.static('client/build'));
 // Express serve up index.html file if it doesn't recognize route
 const path = require('path');
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'NM2', 'build', 'index.html'));
 });
 }
 
