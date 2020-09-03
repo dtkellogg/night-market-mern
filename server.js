abd83f3ab7389bf85292af473a00b6bb0328e39f
Toshi <dtkellogg10@gmail.com>
2020-09-03 01:02:55 -0700

    Attempting to fix error w heroku cannot get path /. Adding piece of code for static file

--- server.js
+++ server.js
@@ -22,6 +22,17 @@ const app = express();
 // // app.engine("handlebars", exphbs());
 // // app.set("view engine", "handlebars");
 
+  if (process.env.NODE_ENV === 'production') {
+// Exprees will serve up production assets
+app.use(express.static('client/build'));
+
+// Express serve up index.html file if it doesn't recognize route
+const path = require('path');
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+}
+
 
 // // Body Parser Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
