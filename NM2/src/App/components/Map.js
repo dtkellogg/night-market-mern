0218121af1400afdd6e52bf135ee538638d83c7a
Toshi <dtkellogg10@gmail.com>
2020-09-14 17:05:42 -0700

    2nd attempt at getting padding-top for pages correct

--- NM2/src/App/components/Map.js
+++ NM2/src/App/components/Map.js
@@ -22,14 +22,14 @@ export default function Map() {
           }
           mapStyle={"mapbox://styles/dtkellogg/ckeisrodw1jfy19uqag1jvobo"}
           onViewportChange={(viewport) => setViewport(viewport)}
-          scrollZoom={false}
-          zoomEnabled={false}
-          rotateEnabled={false}
-          touchZoomRotate={false}
+          // scrollZoom={false}
+          // zoomEnabled={false}
+          // rotateEnabled={false}
+          // touchZoomRotate={false}
           dragPan={false} // This disable moving around the map w 1 finger on mobile
-          drag={false}
-          userInteractionEnabled={'NO'}
-          showCompass={true}
+          // drag={false}
+          // userInteractionEnabled={'NO'}
+          // showCompass={true}
         >
           <Marker latitude={38.546731} longitude={-121.749011}>
             <div className="text-size-2">Davis Night Market</div>
